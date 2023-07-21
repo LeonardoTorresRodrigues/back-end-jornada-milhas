dea01e1dffe0b55d5e853a64cebed191a8427d74
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-07-21 13:52:39 -0300

    feat: add testimonials model

---
+++ src/models/testimonialsModel.js
@@ -0,0 +1,10 @@
+const connection = require('./connection');
+
+const getTestimonials = async () => {
+	const testimonials = await connection.execute('SELECT * FROM testimonials');
+	return testimonials;
+};
+
+module.exports = {
+	getTestimonials
+};
